f97e3823887c7f58fe5e319e901d11f79c84d8d5
Carlos de la Cruz <carlosdelacruzacosta4@gmail.com>
2020-11-07 18:56:26 -0600

    display options and more

--- app.js
+++ app.js
@@ -25,8 +25,25 @@ class UI {
         // parent.appendChild(imagen);
 
         parent.innerHTML = `
-        <div class="delete" data-isbn=${isbn}>&times</div>
-        <img src=${img} alt="Book cover">
+        <div class="iconos">
+                <svg xmlns="http://www.w3.org/2000/svg" id="moreInfo" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+
+                <svg xmlns="http://www.w3.org/2000/svg" id="editInfo" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                </svg>
+
+                <svg xmlns="http://www.w3.org/2000/svg" id="deleteBook" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+            </div>
         <div class="data-wrapper">
          <h2>${title}</h2>
          <p>${author}</p>
@@ -34,7 +51,7 @@ class UI {
         <div>
         `;
 
-
+        parent.style.backgroundImage = `url(${img})`;
 
         parent.classList.add("libro");
 
@@ -54,8 +71,6 @@ class UI {
             UI.gridOff();
         }
     }
-
-
 
     static gridOn() {
         document.querySelector("#message").classList.add("active");
@@ -85,12 +100,16 @@ class UI {
         document.querySelector("#autor").value = "";
         document.querySelector("#ISBN").value = "";
     }
+
+
+    static displayOptions(e) {
+        if (e.target.classList.contains("libro")) {
+            e.target.children[0].classList.add("active");
+        }
+    }
 }
 
 class Store {
-
-
-
     static getBooks() {
         let books;
         if (localStorage.getItem("books") === null) {
@@ -178,4 +197,11 @@ document.addEventListener('DOMContentLoaded', function () {
     UI.displaySavedBooks();
 });
 
+document.querySelector(".bookshelf-wrapper").addEventListener("mouseover", (e) => UI.displayOptions(e));
 
+// document.querySelector(".bookshelf-wrapper").addEventListener("mouseover", (e) => {
+//     console.log(e.target.children[0]);
+//     if (e.target.classList.contains("libro")) {
+//         e.target.children[0].classList.add("active");
+//     }
+// })
